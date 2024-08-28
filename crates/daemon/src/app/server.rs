9de3032223fffc89b0ba966143760e4fed4d115f
Jens Meindertsma <mail@jensmeindertsma.com>
2024-08-28 14:27:42 +0000

    stash

--- crates/daemon/src/app/server.rs
+++ crates/daemon/src/app/server.rs
@@ -41,6 +41,55 @@ impl ServerHandler {
             configuration,
         }
     }
+
+    pub async fn serve_connection<S, F>(
+        &self,
+        mut connection: SocketConnection,
+    ) -> Result<(), Infallible>
+    where
+        S: tower::Service<SocketRequest, Response = SocketResponse, Error = Infallible, Future = F>,
+        F: Future<Output = Result<SocketResponse, Infallible>>,
+    {
+        info!("handling connection from address {:?}", connection.address);
+
+        loop {
+            let result = connection.accept().await;
+            let message = match result {
+                Ok(maybe_message) => match maybe_message {
+                    None => break,
+                    Some(message) => message,
+                },
+                Err(error) => {
+                    error!("failed to read incoming message: {error:?}");
+                    continue;
+                }
+            };
+
+            info!(
+                id = message.id,
+                "received socket request = {:?}", message.request,
+            );
+
+            let response = service.call(message.request).await?;
+
+            info!(
+                id = message.id,
+                "replying to socket request = {:?}", response
+            );
+
+            if let Err(error) = connection
+                .reply(SocketReply {
+                    regarding: message.id,
+                    response,
+                })
+                .await
+            {
+                error!("failed to send reply: {error:?}")
+            };
+        }
+
+        Ok(())
+    }
 }
 
 impl tower::Service<HyperRequest<Incoming>> for ServerHandler {
--- crates/daemon/src/socket.rs
+++ crates/daemon/src/socket.rs
@@ -2,7 +2,6 @@ mod connection;
 mod service;
 
 pub use connection::SocketConnection;
-pub use service::serve_connection;
 use std::{
     env::{self, VarError},
     os::fd::FromRawFd,
