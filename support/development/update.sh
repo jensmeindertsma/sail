d3a370d2068f8a99f187723257121ed8ad2847f9
Jens Meindertsma <mail@jensmeindertsma.com>
2024-10-29 21:00:54 +0000

    Fix update script

--- support/development/update.sh
+++ support/development/update.sh
@@ -5,9 +5,11 @@ echo "Updating service binaries!"
 CLI_PATH="/usr/local/bin/sail"
 DAEMON_PATH="/usr/local/bin/saild"
 
+sudo systemctl stop sail
+
 sudo cp "target/debug/sail" "$CLI_PATH"
 sudo cp "target/debug/saild" "$DAEMON_PATH"
 
-sudo systemctl restart sail
+sudo systemctl start sail
 
 echo "Done!"
