776263c7571dd7802e55c833c9dd9a8425ec20ca
Jens Meindertsma <mail@jensmeindertsma.com>
2025-05-10 13:57:33 +0000

    Remove comma

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -31,7 +31,7 @@ async fn main() -> impl Termination {
         }
     };
 
-    tracing::info!("loaded configuration",);
+    tracing::info!("loaded configuration");
 
     // We spawn a task here that is responsible for accepting and handling
     // new connections and messages coming in over the socket.
