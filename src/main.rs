3232a2fb903e9025a65f6dc074b619018abb7f52
Jens Meindertsma <mail@jensmeindertsma.com>
2025-06-27 14:13:47 +0000

    start drafting commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+
+    // TODO: decide on arguments
+
+    // empty or status
+    // help
+    // new
+    // view <name>
+    // deploy <name>
+    // restart <name>
+    // stop <name>
+    // delete <name>
+    // rename <name>
+    // edit <name> (opens list allowing property to modify to be selected)
+    // - domains
+    // - fallback page
 }
