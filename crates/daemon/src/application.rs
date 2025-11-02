d7315d76695f9a38b490d74ff9d905a522ee264c
Jens Meindertsma <mail@jensmeindertsma.com>
2025-11-02 07:42:43 +0000

    Set up exit handling

--- crates/daemon/src/application.rs
+++ crates/daemon/src/application.rs
@@ -1,12 +1,47 @@
-use tokio::time::{self, Duration};
-use tracing::instrument;
-
-#[instrument(name = "daemon")]
-pub async fn run() {
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+};
+use tokio::{
+    signal::unix::{SignalKind, signal},
+    time::{self, Duration},
+};
+#[tracing::instrument(name = "daemon")]
+pub async fn run() -> Result<(), Failure> {
     tracing::info!("starting up");
 
+    let mut sigterm = signal(SignalKind::terminate()).map_err(|_| Failure::SignalListener)?;
+
     loop {
-        time::sleep(Duration::from_secs(10)).await;
-        tracing::info!("running");
+        tokio::select! {
+            _ = time::sleep(Duration::from_secs(10)) => {
+                tracing::info!("running");
+            }
+
+            _ = sigterm.recv() => {
+                tracing::info!("received SIGTERM, shutting down gracefully");
+                break;
+            }
+
+        }
+    }
+
+    tracing::info!("cleanup done, exiting");
+
+    Ok(())
+}
+
+#[derive(Debug)]
+pub enum Failure {
+    SignalListener,
+}
+
+impl Display for Failure {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::SignalListener => write!(f, "failed to create signal listener"),
+        }
     }
 }
+
+impl Error for Failure {}
--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -1,17 +1,26 @@
 mod application;
 
+use std::process::{ExitCode, Termination};
+
 use tokio::runtime::Builder;
 
-fn main() {
+fn main() -> impl Termination {
     tracing_subscriber::fmt()
         .with_target(false)
         .with_timer(tracing_subscriber::fmt::time::uptime())
         .with_level(true)
         .init();
 
-    Builder::new_multi_thread()
+    match Builder::new_multi_thread()
         .enable_all()
         .build()
         .unwrap()
         .block_on(application::run())
+    {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(error) => {
+            tracing::error!("daemon crashed: {error}");
+            ExitCode::FAILURE
+        }
+    }
 }
